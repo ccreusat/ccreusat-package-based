2b80294cae0d1e1c7dcb81b415ab9f69876cb8e5
Clement Creusat <clement.creusat@gmail.com>
2024-10-06 18:05:22 +0200

    feat(some-lib): add client to some-lib

--- packages/some-lib/src/lib/some-lib.tsx
+++ packages/some-lib/src/lib/some-lib.tsx
@@ -1,6 +1,8 @@
+import { client } from '@ccreusat/client';
 import styles from './some-lib.module.css';
 
 export function SomeLib() {
+  console.log({ client });
   return (
     <div className={styles['container']}>
       <h1>Welcome to SomeLib!</h1>

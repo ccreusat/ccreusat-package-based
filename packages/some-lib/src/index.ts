9499c8e8b316db31f02180299358b91eebf558b8
Clement Creusat <clement.creusat@gmail.com>
2024-10-24 17:56:34 +0200

    feat(some-lib): add new component bd

--- packages/some-lib/src/index.ts
+++ packages/some-lib/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/bd/bd';
 export * from './lib/publi/publi';
 export * from './lib/test-2/test-2';
 export * from './lib/test/test';
---
+++ packages/some-lib/src/lib/bd/bd.tsx
@@ -0,0 +1,11 @@
+import styles from './bd.module.css';
+
+export function Bd() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Bd!</h1>
+    </div>
+  );
+}
+
+export default Bd;

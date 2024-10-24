4d7728f6bbd3dd7f57ad052726262f35c9cc89db
Clement Creusat <clement.creusat@gmail.com>
2024-10-24 17:16:53 +0200

    feat(some-lib): add new component

--- packages/some-lib/src/index.ts
+++ packages/some-lib/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/publi/publi';
 export * from './lib/test-2/test-2';
 export * from './lib/test/test';
 export * from './lib/some-lib';
---
+++ packages/some-lib/src/lib/publi/publi.tsx
@@ -0,0 +1,11 @@
+import styles from './publi.module.css';
+
+export function Publi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Publi!</h1>
+    </div>
+  );
+}
+
+export default Publi;

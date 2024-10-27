dd344e64979b889f1d3d1f796d896112ef96af01
Clement Creusat <clement.creusat@gmail.com>
2024-10-27 18:31:18 +0100

    feat(client): remove tracking ts

--- packages/client/src/lib/tracking.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { http } from './http';
-
-describe('http', () => {
-  it('should work', () => {
-    expect(http()).toEqual('http');
-  });
-});
